f7de02c4658fd67bc743928fb55ce1052b682b50
unknown <vijaypatole0208@gmail.com>
2023-06-01 09:22:25 +0530

    update emaildetails

---
+++ src/emailAap/Emaildetails.java
@@ -0,0 +1,13 @@
+package emailAap;
+
+public class Emaildetails {
+
+	public static void main(String[] args) {
+		int num=10;
+		int num1=20;
+	int sum= num +num1;
+	System.out.println(sum);
+
+	}
+
+}
